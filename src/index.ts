11e10c75a90ec5de0893a81fe8584ea0d87f414c
Vitali Logvin <vitali.logvin@noogadev.com>
2021-11-25 18:09:54 +0300

    worckspace setting

---
+++ src/index.ts
@@ -0,0 +1,71 @@
+import './style.css'
+import { debounceTime, delay, fromEvent, Observable, scan, throttleTime } from 'rxjs'
+import { onErrorResumeNext } from 'rxjs/operators'
+
+const btn1 = document.getElementById('btn1')
+const btn2 = document.getElementById('btn2')
+const result = document.getElementById('result')
+
+
+// ======================== fromEvent (addEventListener)
+
+
+// fromEvent(btn1, 'click').subscribe(() => result.innerText = 'was clicked btn1')
+// fromEvent(btn2, 'click').subscribe(() => result.innerText = 'was clicked btn2')
+
+
+// ========================= pipe, throttleTime, delay, debounceTime
+
+
+// fromEvent(btn1, 'click')
+//     .pipe(
+//         throttleTime(1000),
+//         scan(count => count + 1, 0)
+//         )
+//     .subscribe((count) => result.innerText = `${count}`)
+
+// fromEvent(btn2, 'click')
+//     .pipe(
+//         delay(2000),
+//         scan(count => count + 1, 0)
+//         )
+//     .subscribe((count) => console.log(`button2 was clicked ${count} times`))
+
+
+// ========================= Observable
+
+interface responseData {
+    name: string;
+    id: number
+}
+
+const observable = new Observable<responseData>(subscriber => {
+    fetch('https://rickandmortyapi.com/api/character/2')
+        .then((resp) => resp.json())
+        .then((data) => {
+            subscriber.next(data)
+            subscriber.complete()
+        })
+})
+
+fromEvent(btn2, 'click').subscribe(() => {
+    observable.subscribe({
+        next(data) {
+            console.log(data.name)
+        },
+        complete() {
+             console.log('complete')
+        }
+     })
+})
+
+fromEvent(btn1, 'click').subscribe(() => {
+    observable.subscribe({
+        next(data) {
+            console.log(data.id)
+        },
+        complete() {
+             console.log('complete')
+        }
+     })
+})
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+  },
+};
