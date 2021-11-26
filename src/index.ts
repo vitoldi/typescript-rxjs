7e705babfea9c5b59fc7fcc9d9ec0a9cad71ede0
Vitali Logvin <vitali.logvin@noogadev.com>
2021-11-26 09:01:01 +0300

    operators

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import './style.css'
-import { debounceTime, delay, fromEvent, Observable, scan, throttleTime } from 'rxjs'
+import { debounceTime, delay, fromEvent, Observable, scan, throttleTime, interval, map, filter, tap, take } from 'rxjs'
 import { onErrorResumeNext } from 'rxjs/operators'
 
 const btn1 = document.getElementById('btn1')
@@ -34,38 +34,58 @@ const result = document.getElementById('result')
 
 // ========================= Observable
 
-interface responseData {
-    name: string;
-    id: number
-}
+// interface responseData {
+//     name: string;
+//     id: number
+// }
 
-const observable = new Observable<responseData>(subscriber => {
-    fetch('https://rickandmortyapi.com/api/character/2')
-        .then((resp) => resp.json())
-        .then((data) => {
-            subscriber.next(data)
-            subscriber.complete()
-        })
-})
+// const observable = new Observable<responseData>(subscriber => {
+//     fetch('https://rickandmortyapi.com/api/character/2')
+//         .then((resp) => resp.json())
+//         .then((data) => {
+//             subscriber.next(data)
+//             subscriber.complete()
+//         })
+// })
 
-fromEvent(btn2, 'click').subscribe(() => {
-    observable.subscribe({
-        next(data) {
-            console.log(data.name)
-        },
-        complete() {
-             console.log('complete')
-        }
-     })
-})
+// fromEvent(btn2, 'click').subscribe(() => {
+//     observable.subscribe({
+//         next(data) {
+//             console.log(data.name)
+//         },
+//         complete() {
+//              console.log('complete')
+//         }
+//      })
+// })
 
-fromEvent(btn1, 'click').subscribe(() => {
-    observable.subscribe({
-        next(data) {
-            console.log(data.id)
-        },
-        complete() {
-             console.log('complete')
-        }
-     })
+// fromEvent(btn1, 'click').subscribe(() => {
+//     observable.subscribe({
+//         next(data) {
+//             console.log(data.id)
+//         },
+//         complete() {
+//              console.log('complete')
+//         }
+//      })
+// })
+
+
+// ======================== Operators
+
+const stream$ = interval(1000)
+ .pipe(
+     tap(() => console.log('hello')), // side effect
+     map((v) => v * 3), // change next value
+     filter((v) => v % 2 === 0), //filter next value
+     take(5) // count all values need to take
+ )
+
+stream$.subscribe({
+    next(v) {
+        console.log(v)
+    },
+    complete() {
+        console.log('complete')
+    }
 })
